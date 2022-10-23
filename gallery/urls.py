c7a58cf6b3a65ab02ccc3d9ef760862b83e82b9c
Grazielly-Araujo <graziellygalvaoaraujo@gmail.com>
2022-10-23 02:02:14 -0300

    html Img

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from gallery.views import *
+from gallery.views import index, imagem
 
 urlpatterns = [
     path('', index),
--- gallery/views.py
+++ gallery/views.py
@@ -5,6 +5,6 @@ def index(request):
     return render(request, 'gallery/index.html')
 
 def imagem(request):
-    return render(request, 'gallery/imagem')
+    return render(request, 'gallery/imagem.html')
 
 # Create your views here.
